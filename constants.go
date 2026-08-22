0152f27c1c0d464c93856675c1492c03ea8ca647
Akmal Dzakwan Faiz <akmal.dz.f@gmail.com>
2026-08-22 14:28:13 +0700

    Constants

---
+++ constants.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const pi = 3.14
+const GRAVITY = 9.81
+
+func main() {
+	const days int = 7
+	const (
+		monday    = 1
+		tuesday   = 2
+		wednesday = 3
+		thursday int = 4
+	)
+
+	name := "john"
+
+	fmt.Println(name)
+}
