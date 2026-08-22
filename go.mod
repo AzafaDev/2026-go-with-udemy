07f58cb828089827cec853cf5861edfb384d75aa
Akmal Dzakwan Faiz <akmal.dz.f@gmail.com>
2026-08-22 13:02:29 +0700

    Variables

---
+++ go.mod
@@ -0,0 +1,3 @@
+module 2026-go-with-udemy
+
+go 1.26.2
---
+++ variables.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+var middleName = "Cane"
+
+func main() {
+	// var age int
+	// var name string = "John"
+	// var name1 = "Jane"
+
+	// count := 10
+	// lastName := "Smith"
+	// middleName := "Cane"
+	// middleName := "Mayor"
+	fmt.Println(middleName)
+
+	// Default Values
+	// Numeric Types: 0
+	// Boolean Types: false
+	// String type: ""
+	// Pointers, Slices, Maps, Functions, Structs: nil (null)
+
+	// --- SCOPE
+
+	// fmt.Println(firstName)
+}
+
+func printName() {
+	firstName := "Michael"
+
+	fmt.Println(firstName)
+}
